7fef07fc1931549621184b9b9132dc93da54fc61
Youssef Ayman <youssef.ayman@robustastudio.com>
2024-08-11 18:54:45 +0300

    feat: add products to the graphql category type

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -18,4 +18,8 @@ class Category extends Model
     public function subCategories(){
         return $this->hasMany(Category::class, "parent_id");
     }
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
