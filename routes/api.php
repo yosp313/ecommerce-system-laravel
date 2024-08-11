31848b07d686120eed41f37ef85d24187a0a37e5
Youssef Ayman <youssef.ayman@robustastudio.com>
2024-08-11 13:28:09 +0300

    refactor: moved all of the api routes to the api.php file

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,9 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get("/csrf", function(){
+    return csrf_token();
+});
+
+
+Route::post("/admin/login", [AdminController::class, "login"]);
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/csrf", function(){
-    return csrf_token();
-});
-
-
-Route::post("/admin/login", [AdminController::class, "login"]);
