762892dcb6b6f10616a52d662f19cdb787f7b971
Youssef Ayman <youssef.ayman@robustastudio.com>
2024-08-11 18:29:32 +0300

    feat: add a graphql endpoint for finding a category and getting all categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,4 +14,8 @@ class Category extends Model
     public function parent(){
         return $this->belongsTo(Category::class, "parent_id");
     }
+
+    public function subCategories(){
+        return $this->hasMany(Category::class, "parent_id");
+    }
 }
